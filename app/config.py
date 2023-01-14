7980b1a803118ad88ecb86056c280e56d8bedd4a
Vitaliy <realvitl@yandex.ru>
2023-01-14 12:33:15 +0300

    add .env file and fix bugs

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ from starlette.datastructures import Secret
 dir_path = os.path.dirname(os.path.realpath(__file__))
 root_dir = dir_path[:-3]
 config = Config(f'{root_dir}.env')
-DATABASE_URL = f'sqlite:///{root_dir}' + config('DB_NAME', cast=str, default='db.sqlite')
+DATABASE_URL = f'sqlite:///{root_dir}' + config('DB_NAME', cast=str, default=os.getenv('DB_NAME', 'db.sqlite'))
 
-SECRET_KEY = config('SECRET_KEY', cast=Secret, default='secret')
+SECRET_KEY = config('SECRET_KEY', cast=Secret, default=os.getenv('SECRET_KEY', 'secret'))
