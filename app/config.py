e54e141b690268312ac350679e382c9cfcc2ecfe
Vitaliy <realvitl@yandex.ru>
2023-01-15 22:26:38 +0300

    move database to db folder

--- app/config.py
+++ app/config.py
@@ -4,8 +4,11 @@ from starlette.config import Config
 from starlette.datastructures import Secret
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-root_dir = dir_path[:-3]
-config = Config(f'{root_dir}.env')
+root_dir = dir_path[:-3] + "db\\"
+print(root_dir)
+print(dir_path)
+config = Config(f'{dir_path[:-3]}.env')
 DATABASE_URL = f'sqlite:///{root_dir}' + config('DB_NAME', cast=str, default=os.getenv('DB_NAME', 'db.sqlite'))
+print(DATABASE_URL)
 
 SECRET_KEY = config('SECRET_KEY', cast=Secret, default=os.getenv('SECRET_KEY', 'secret'))
