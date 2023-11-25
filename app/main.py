481f899b00968bf70a9195d00953db512a70b6aa
Vitali Shchamialiou <94484769+To-n-y@users.noreply.github.com>
2023-11-25 14:29:31 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,5 +13,29 @@ def create_app():
 
 
 app = create_app()
+
+@app.websocket("/ws")
+async def websocket(websocket: WebSocket):
+    await websocket.accept()
+    data = await websocket.receive_text()
+    await websocket.send_text(f"You sent: {data}")
+    await websocket.close()
+
+
+@app.websocket("/ws/{chat_name}")
+async def websocket_endpoint(websocket: WebSocket, chat_name: str):
+    await websocket.accept()
+    if chat_name not in chat_rooms:
+        chat_rooms[chat_name] = []
+    chat_rooms[chat_name].append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for ws in chat_rooms[chat_name]:
+                if ws != websocket:
+                    await ws.send_text(data)
+    except WebSocketDisconnect:
+        chat_rooms[chat_name].remove(websocket)
+        
 app.include_router(router)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
